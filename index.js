ec053d6c5b64a60ce87958875f9d1605eaf6be38
Joël Birrer <birrer.joel@hotmail.ch>
2020-07-21 21:25:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hi");
+if (2 > 1) {
+    var newLocal = 3;
+    console.log(newLocal);
+}
+document.getElementById("maiden").innerText = "12";
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("hi")
+
+if (2>1){
+    const newLocal = 3;
+    console.log(newLocal)
+}
+
+document.getElementById("maiden")!.innerText = "12"
